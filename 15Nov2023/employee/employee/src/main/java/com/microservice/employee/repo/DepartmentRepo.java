fa93de6c3291ecb0bb4d14e677361889f19b6498
SaisunilAkula <149362938+SaisunilAkula@users.noreply.github.com>
2023-11-16 11:08:03 +0530

    Delete 15Nov2023 directory

--- 15Nov2023/employee/employee/src/main/java/com/microservice/employee/repo/DepartmentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.microservice.employee.repo;
-
-import com.microservice.employee.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepo extends JpaRepository<Department,Long> {
-
-}
--- 15Nov2023/employee/employee/src/main/java/com/microservice/employee/repo/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.microservice.employee.repo;
-
-import com.microservice.employee.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long> {
-}
